2ab995209f22849e7a3c80576c5894571854871d
Eason <ejin458@aucklanduni.ac.nz>
2023-08-30 14:35:13 +1200

    turned timetable the right way around

--- task-list/src/components/TimeTable/TimeTable.js
+++ task-list/src/components/TimeTable/TimeTable.js
@@ -2,29 +2,40 @@ import React from 'react';
 import './TimeTable.css';
 
 const daysOfWeek = ['', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-const tasks = [['1'], ['2'], ['3'], ['4'], ['5']];
+const taskList = [['1'], ['2'], ['3'], ['4'], ['5']];
 const timeSlots = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM'];
+
+/* TODO: refactor tasks as object and time as hashmap
+task will have fields for name, time, and day
+time will have time as key and array of tasks as value
+*/
 
 const Timetable = () => {
   return (
     <div className="timetable">
-      {daysOfWeek.map((day) => (
-        <div key={day} className="cell day-header">
-          {day}
-        </div>
-      ))}
-      {timeSlots.map((time) => (
-        <div key={time} className="time-row">
-          <div className="cell time-header">{time}</div>
-          {tasks.map((task) => (
-            <div key={task} className="cell">
-              {task}
-            </div>
+      <table>
+        <tr>
+          {daysOfWeek.map((day, index) => (
+            <th key={index}>{day}</th>
           ))}
-        </div>
-      ))}
+        </tr>
+        <tbody>
+          {taskList.map((tasks, index) => (
+            <tr key={index}>
+              <td>{timeSlots[index]}</td>
+              {daysOfWeek.map((day, index) => (
+                <td key={index}>
+                  {tasks.map((task, index) => (
+                    task === null? <div key={index}></div> : <div key={index}>{task}</div>
+                  ))}
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
-};
+}
 
 export default Timetable;
