edd30446a1003a9b63e7b6ed4b7c0d698adc49ed
Eason <ejin458@aucklanduni.ac.nz>
2023-09-19 17:34:04 +1200

    New timetable (placeholder tasks)

--- task-list/src/components/TimeTable/TimeTable.js
+++ task-list/src/components/TimeTable/TimeTable.js
@@ -1,41 +1,130 @@
-import React from 'react';
-import './TimeTable.css';
+import React, { useState } from "react";
+import "./TimeTable.css";
 
-const daysOfWeek = ['', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-const taskList = [['1'], ['2'], ['3'], ['4'], ['5']];
-const timeSlots = ['9 AM', '10 AM', '11 AM', '12 PM', '1 PM', '2 PM', '3 PM'];
+const TimeTable = () => {
+  const [tasks, setTasks] = useState([
+    {
+      day: "Monday",
+      tasks: [
+        {
+          id: 1,
+          task: "Task 1",
+          time: "9:00 AM",
+          completed: false,
+        },
+        {
+          id: 2,
+          task: "Task 2",
+          time: "9:00 AM",
+          completed: false,
+        },
+      ],
+    },
+    {
+      day: "Tuesday",
+      tasks: [
+        {
+          id: 3,
+          task: "Task 3",
+          time: "10:00 AM",
+          completed: false,
+        },
+        {
+          id: 4,
+          task: "Task 4",
+          time: "10:00 AM",
+          completed: false,
+        },
+      ],
+    },
+    {
+      day: "Wednesday",
+      tasks: [
+        {
+          id: 5,
+          task: "Task 5",
+          time: "11:00 AM",
+          completed: false,
+        },
+        {
+          id: 6,
+          task: "Task 6",
+          time: "11:00 AM",
+          completed: false,
+        },
+      ],
+    },
+    {
+      day: "Thursday",
+      tasks: [
+        {
+          id: 7,
+          task: "Task 7",
+          time: "12:00 PM",
+          completed: false,
+        },
+      ],
+    },
+    {
+      day: "Friday",
+      tasks: [],
+    },
+  ]);
 
-/* TODO: refactor tasks as object and time as hashmap
-task will have fields for name, time, and day
-time will have time as key and array of tasks as value
-*/
+  const handleCheckboxChange = (id, day) => {
+    const newTasks = tasks.map((taskDay) => {
+      if (taskDay.day === day) {
+        taskDay.tasks = taskDay.tasks.map((task) => {
+          if (task.id === id) {
+            task.completed = !task.completed;
+          }
+          return task;
+        });
+      }
+      return taskDay;
+    });
+    setTasks(newTasks);
+  };
 
-const Timetable = () => {
   return (
-    <div className="timetable">
+    <div className="time-table">
       <table>
-        <tr>
-          {daysOfWeek.map((day, index) => (
-            <th key={index}>{day}</th>
-          ))}
-        </tr>
+        <thead>
+          <tr>
+            <th>Time</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+          </tr>
+        </thead>
         <tbody>
-          {taskList.map((tasks, index) => (
-            <tr key={index}>
-              <td>{timeSlots[index]}</td>
-              {daysOfWeek.map((day, index) => (
-                <td key={index}>
-                  {tasks.map((task, index) => (
-                    task === null? <div key={index}></div> : <div key={index}>{task}</div>
-                  ))}
-                </td>
-              ))}
-            </tr>
-          ))}
+          {Array(7)
+            .fill(null)
+            .map((_, index) => (
+              <tr key={index}>
+                <td>{index + 9}:00 AM</td>
+                {tasks.map((taskDay) => (
+                  <td key={taskDay.day}>
+                    {taskDay.tasks.filter((task) => task.time === `${index + 9}:00 AM`).map((task) => (
+                      <div key={task.id}>
+                        <input
+                          type="checkbox"
+                          checked={task.completed}
+                          onChange={() => handleCheckboxChange(task.id, taskDay.day)}
+                        />
+                        {task.task}
+                      </div>
+                    ))}
+                  </td>
+                ))}
+              </tr>
+            ))}
         </tbody>
       </table>
     </div>
   );
-}
+};
 
-export default Timetable;
+export default TimeTable;
