2ca299d379fd6974b78e23a09d38d827bfa33cd7
Eason <ejin458@aucklanduni.ac.nz>
2023-11-02 14:46:38 +1300

    Clear completed

--- task-list/src/components/TimeTable/Completed.js
+++ task-list/src/components/TimeTable/Completed.js
@@ -9,9 +9,18 @@ const Completed = ({ completedTasks, setCompletedTasks }) => {
     }
   });
 
+  const handleClearCompleted = () => {
+    // Clear the completed list in state
+    setCompletedTasks([]);
+    
+    // Clear the completed list in local storage
+    localStorage.removeItem("completedTasks");
+  };
+
   return (
     <div>
       <h1>Completed</h1>
+      <button onClick={handleClearCompleted}>Clear Completed</button>
       <ul>
         {completedTasks.map((task, index) => (
           <li key={index}>
